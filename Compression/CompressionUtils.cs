0beaf2453abc19d9ca0d513dd309f4baf3367c1e
Aidan Hutchinson <aidan.hutchinson@manpower.nl>
2023-04-24 13:46:59 +0100

    Add project files.

---
+++ Compression/CompressionUtils.cs
@@ -0,0 +1,35 @@
+﻿using System.IO.Compression;
+
+namespace Compression
+{
+    public static class CompressionUtils
+    {
+        public static void CompressFile(string inputFilePath, string outputFilePath)
+        {
+            using (var inputFileStream = File.OpenRead(inputFilePath))
+            {
+                using (var outputFileStream = File.Create(outputFilePath))
+                {
+                    using (var deflateStream = new DeflateStream(outputFileStream, CompressionLevel.Optimal))
+                    {
+                        inputFileStream.CopyTo(deflateStream);
+                    }
+                }
+            }
+        }
+
+        public static void DecompressFile(string inputFilePath, string outputFilePath)
+        {
+            using (var inputFileStream = File.OpenRead(inputFilePath))
+            {
+                using (var outputFileStream = File.Create(outputFilePath))
+                {
+                    using (var deflateStream = new DeflateStream(inputFileStream, CompressionMode.Decompress))
+                    {
+                        deflateStream.CopyTo(outputFileStream);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Compression/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Compression;
+
+Console.WriteLine("Compression example started...");
+
+const string inputImagePath = "testimage.bmp";
+const string compressedFilePath = "compressed.bin";
+const string decompressedImagePath = "decompressed.bmp";
+
+CompressionUtils.CompressFile(inputImagePath, compressedFilePath);
+Console.WriteLine("Image compression complete.");
+
+CompressionUtils.DecompressFile(compressedFilePath, decompressedImagePath);
+Console.WriteLine("Image decompression complete.");
+
